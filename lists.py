eab7b03781ea424db2528d5eaeb0815fbaf1c643
spin-neo <mpfogliato@mail.austral.edu.ar>
2025-07-14 17:22:13 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,6 +1,3 @@
-# Replace the "ANSWER HERE" with your answer
-
-
 def remove_elements(list_to_remove_elements):
     indices_to_remove = [0, 4, 5]
     return [item for i, item in enumerate(list_to_remove_elements) if i not in indices_to_remove]
@@ -15,10 +12,7 @@ def is_empty(list_to_check):
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
-        return list_to_compare1[2] == list_to_compare2[2]
-    else:
-        return False
+    return sorted(list_to_compare1) == sorted(list_to_compare2)
 
 
 def list_of_lists(list_of_lists_to_modify):
@@ -29,4 +23,4 @@ def list_of_lists(list_of_lists_to_modify):
         modified_list.append(list_of_lists_to_modify[1][1:4])
     if len(list_of_lists_to_modify) >= 3:
         modified_list.append(list_of_lists_to_modify[2][-2:])
-    return modified_list
+    return modified_list
